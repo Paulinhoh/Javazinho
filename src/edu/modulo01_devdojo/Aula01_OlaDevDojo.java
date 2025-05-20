4eb863369d81206379e1c37d96528be27a71037f
Paulo Henrique <phdsreis@gmail.com>
2025-05-19 22:37:34 -0300

    feat: dia 1 - configs iniciais e tipos primitivos

---
+++ src/edu/modulo01_devdojo/Aula01_OlaDevDojo.java
@@ -0,0 +1,19 @@
+package edu.modulo01_devdojo;
+
+public class Aula01_OlaDevDojo {
+
+    // Comentarios em linha
+
+    /*
+    * Comentarios em
+    * multiplas linhas
+    */
+
+    /**
+     * java doc - comentario para documentação
+     */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
---
+++ src/edu/modulo01_devdojo/Aula02_TiposPrimitivos.java
@@ -0,0 +1,31 @@
+package edu.modulo01_devdojo;
+
+public class Aula02_TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 23;
+        long bigNumber = 100000L;
+        double salaryDouble = 2000.0;
+        float salaryFloat = 2500.0F;
+        byte ageByte = 10; // -128 até 127
+        short ageShort = 10;
+        boolean trueOrFalse = true;
+        char letter = 'c';
+
+        System.out.println("Minha idade é "+age);
+        System.out.println(trueOrFalse);
+        System.out.println("char: "+letter);
+
+        // cast
+        long number = (long) 123.50;
+        System.out.println("Numero Double convertido para Long: "+number);
+
+        // String
+        String name = "Paulo Henrique"; // String não é um tipo primitivo e sim um Objeto
+        System.out.println(name);
+
+        // Tem uma maneira de criar uma variavel omitindo o seu tipo
+        var lastName = "Paulo Henrique";
+        System.out.println(lastName);
+    }
+}
