a15e659d09889b9434b518c70ac94bc8fac63c23
Paulo Henrique <phdsreis@gmail.com>
2025-07-27 21:51:59 -0300

    feat: construtores

---
+++ src/edu/modulo01_devdojo/javacore/D_construtores/test/AnimeTest01.java
@@ -0,0 +1,15 @@
+package edu.modulo01_devdojo.javacore.D_construtores.test;
+
+import edu.modulo01_devdojo.javacore.D_construtores.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Haikyuu", "Tv", 12, "Ação", "Production IG");
+        anime.imprime();
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-==-==-=-=-=");
+
+        Anime anime2 = new Anime();
+        anime2.imprime();
+    }
+}
