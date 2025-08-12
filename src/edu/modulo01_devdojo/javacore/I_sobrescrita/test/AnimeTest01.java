33137d87ac162e7959f7382cbf2c9f25bd624529
Paulo Henrique <phdsreis@gmail.com>
2025-08-11 23:18:19 -0300

    feat: sobrescrita do método toString()

---
+++ src/edu/modulo01_devdojo/javacore/I_sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package edu.modulo01_devdojo.javacore.I_sobrescrita.test;
+
+import edu.modulo01_devdojo.javacore.I_sobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+
+    }
+}
