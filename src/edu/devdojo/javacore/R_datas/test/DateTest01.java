12f9e276a09114feda5bca292ebd5948b934515a
Paulo Henrique <phdsreis@gmail.com>
2025-08-18 14:26:39 -0300

    feat: classes utilitarias - date

---
+++ src/edu/devdojo/javacore/R_datas/test/DateTest01.java
@@ -0,0 +1,13 @@
+package edu.devdojo.javacore.R_datas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        // Classe ja obsoleta -> utilizado somente em sistemas legados
+        Date date = new Date(1_000_000_000);        // long
+        date.setTime(date.getTime() + 3_600_000);   // acrescentando 1h
+
+        System.out.println(date);
+    }
+}
